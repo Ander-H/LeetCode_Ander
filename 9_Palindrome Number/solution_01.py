d5b9a78b7bf8632050b03fec9d6c0b5edefd04c7
Ander <majupo@126.com>
2018-03-31 11:07:05 +0800

    9 solution_01 accept

--- 9_Palindrome Number/solution_01.py
+++ 9_Palindrome Number/solution_01.py
@@ -1,2 +1,14 @@
-# hello, git
-# I'm Ander
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        for i in range(round(len(str(x)) / 2)):
+            if str(x)[i] != str(x)[-i-1]:
+                return False
+        return True
+
+a = Solution()
+print(a.isPalindrome(1000021))
+print('###')
