c17e6602d7a00d5e29da49714f080d72e026a1aa
ander <majupo@126.com>
2018-06-28 00:35:34 +0800

    20180628 update

--- 4_Media of Two Sorted Arrays/review_01.py
+++ 4_Media of Two Sorted Arrays/review_01.py
@@ -10,14 +10,14 @@ class Solution:
         if len(nums1) > len(nums2):
             nums1, nums2, m, n = nums2, nums1, n, m
         half_len = int((m+n+1)/2)
-        imin, imax = 0, len(nums1)-1
+        imin, imax = 0, len(nums1)
 
-        while True:
+        while imin < imax:
             i = int((imin + imax) / 2)
-            j = half_len - i - 2
-            if nums1[i] > nums2[j + 1] and i != m:
-                imax = i
-            elif nums1[i+1]  < nums2[j] and i != m:
+            j = half_len - i
+            if i > 0 and nums1[i] > nums2[j] :
+                imax = i - 1
+            elif i < m and nums1[i+1] < nums2[j-1]:
                 imax = i + 1
             else:
                 if (m+n)%2 != 0:
